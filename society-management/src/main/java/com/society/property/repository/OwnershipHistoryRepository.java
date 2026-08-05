176777f0a1d4a3b7b641eda943ee4c52900b35fc
Pawan <kollurupawankumar@gmail.com>
2026-08-05 18:03:40 +0530

    fix(property): add @Param annotation to findByPropertyId in OwnershipHistoryRepository

--- society-management/src/main/java/com/society/property/repository/OwnershipHistoryRepository.java
+++ society-management/src/main/java/com/society/property/repository/OwnershipHistoryRepository.java
@@ -2,11 +2,8 @@ package com.society.property.repository;
 
 import com.society.property.entity.OwnershipHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,7 +12,7 @@ import java.util.List;
 public interface OwnershipHistoryRepository extends JpaRepository<OwnershipHistoryEntity, Integer> {
 
     @Query("SELECT h FROM OwnershipHistoryEntity h LEFT JOIN FETCH h.member WHERE h.property.id = :propertyId ORDER BY h.id DESC")
-    List<OwnershipHistoryEntity> findByPropertyId(Integer propertyId);
+    List<OwnershipHistoryEntity> findByPropertyId(@Param("propertyId") Integer propertyId);
 
     List<OwnershipHistoryEntity> findByMemberId(Integer memberId);
 }
